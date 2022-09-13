d64054b87cd18fa8be06b26c23ba13a243fd4a2b
OrionNavattan <103385399+OrionNavattan@users.noreply.github.com>
2022-09-12 21:05:16 -0700

    File renames, variables, and beginning to work on SonLVL defs

--- RAM Addresses.asm
+++ RAM Addresses.asm
@@ -311,7 +311,7 @@ unused_F61A:		rs.l 1 ; $FFFFF61A ; only ever cleared, never used
 v_fg_y_pos_vsram_p2:		rs.w 1 ; $FFFFF61E ; foreground y position for player 2
 v_bg_y_pos_vsram_p2:		rs.w 1 ; $FFFFF620 ; background y position for player 2
 v_teleport_timer:			rs.b 1 ; $FFFFF622 ; timer for teleport effect
-f_teleport_flag:			rs.b 1 ; $FFFFF623 ; flag to indicate a teleport is in progress
+f_teleport_flag:			rs.b 1 ; $FFFFF623 ; flag indicating a teleport is in progress
 v_vdp_hint_counter:			rs.w 1 ; $FFFFF624 ; ; VDP register $8A buffer - horizontal interrupt counter ($8Axx)
 v_palfade_start:			rs.b 1 ; $FFFFF626 ; palette fading - start position in bytes
 v_palfade_size:				rs.b 1 ; $FFFFF627 ; palette fading - number of colors
@@ -370,8 +370,7 @@ v_sonic_look_delay_counter:	rs.w 1 ; $FFFFF66C ; timer for delay until Sonic loo
 v_tails_look_delay_counter:	rs.w 1 ; $FFFFF66E ; timer for delay until Tails looks up while up is pressed
 v_super_sonic_frame_count:	rs.w 1 ; $FFFFF670 
 v_camera_arz_bg_x_pos:		rs.l 1 ; $FFFFF672
-
-unused_F676:		rs.b $A	; $FFFFF676-$FFFFF67F ; seems unused
+unused_F676:				rs.b $A	; $FFFFF676-$FFFFF67F ; seems unused
 					rsblockend misc_level_variables
 
 					rsblock plc_buffer_and_regs
@@ -520,7 +519,7 @@ unused_F7C8:				rs.b 1 ; $FFFFF7C8 ; seems unused
 f_wind_tunnel_disable:		rs.b 1 ; $FFFFF7C9 ; flag indicating Sonic is holding onto something in a wind tunnel
 unused_F7CA:				rs.b 2 ; $FFFFF7CA-$FFFFF7CB ; seems unused
 f_lock_controls:			rs.b 1 ; $FFFFF7CC ; flag set to lock player 1 controls
-f_ss_2p:					rs.b 1 ; $FFFFF7CD ; flag to indicate Special Stage 2-player mode
+f_unused_ss_flag:			rs.b 1 ; $FFFFF7CD ; flag set by 2P results screen and star post, but never read or used
 unused_F7CE:				rs.b 1 ; $FFFFF7CE ; seems unused
 f_lock_controls_p2:			rs.b 1 ; $FFFFF7CF ; flag set to lock player 2 controls
 v_enemy_combo:				rs.w 1 ; $FFFFF7D0 ; number of enemies/blocks broken in a row, times 2
@@ -538,7 +537,7 @@ v_camera_x_pos_coarse_p2_end:		   ; required for teleport swap table
 
 v_tails_last_frame_id:			rs.b 1 ; $FFFFF7DE ; Tails' previous frame id; compared with current frame to determine if graphics need updating. can be set to a dummy value like -1 to force a refresh DMA.
 v_tailstails_last_frame_id:		rs.b 1 ; $FFFFF7DF ; Previous frame id of Tails' tails; compared with current frame to determine if graphics need updating. can be set to a dummy value like -1 to force a refresh DMA.
-v_buttonvine_triggers:			rs.b $10	; $FFFFF7E0 ; 16 byte flag array, #subtype byte set when button/vine of respective subtype activated
+v_button_state:			rs.b $10	; $FFFFF7E0 ; 16 byte flag array, #subtype byte set when button/vine of respective subtype activated
 v_anim_counters:				rs.b $10	; $FFFFF7F0-$FFFFF7FF
 				rsblockend misc_variables
 
@@ -574,13 +573,245 @@ v_stack:					rs.b $140 ; $FFFFFCC0 ; system stack; the first $BE bytes are clear
 v_stack_pointer:			rs.w 1  ; $FFFFFE00 ; initial stack pointer - items are added to the stack backwards from this address
 v_keep_after_reset:         equ v_stack_pointer ; $FFFFFE00 ; everything after this address is kept in RAM after a soft reset
 
+f_ss_2p_flag:				equ v_stack_pointer ; $FFFFFE00-$FFFFFE01 ; flag indicating Special Stage 2P mode
+f_restart:					rs.w 1 ; $FFFFFE02 ; flag set to end/restart level(2 bytes)
+v_frame_counter:			rs.w 1 ; $FFFFFE04 ; frame counter, increments every frame
+v_debug_item_index:			rs.b 1 ; $FFFFFE06 ; debug item currently selected (NOT the object id of the item)
+
+unused_FE07:	rs.b 1	; $FFFFFE07 ; seems unused
+
+v_debug_active:				rs.w 1 ; $FFFFFE08 ; high byte is the debug mode routine counter ; low byte is flag: xx01 when debug mode is in use and Sonic is an item; 0 otherwise
+v_debug_move_delay:			rs.b 1 ; $FFFFFE0A ; debug mode - time delay in frames between holding the d-pad and the object moving
+v_debug_move_speed:			rs.b 1 ; $FFFFFE0B ; debug mode - speed the object moves, increases the longer d-pad is held
+v_vblank_counter:			rs.l 1 ; $FFFFFE0C ; vertical interrupt counter, increments every VBlank
+
+v_zone:						rs.w 1 ; $FFFFFE10 ; high byte is current zone number, low byte is current act number ; read as a byte if we only want the zone
+v_act:						equ __rs-1 ; $FFFFFE11 ; read if we only want the act number
+v_lives:					rs.b	1 ; $FFFFFE12 ; number of lives
+
+unused_FE13:	rs.b	3 ; $FFFFFE13-$FFFFFE15 ; unused ($FE14 was used by the air counter in Sonic 1, which is now implemented in the player's object RAM)
+
+v_special_stage:			rs.w	1 ; $FFFFFE16 ; high byte is ID of current special stage, low byte is current act of current special stage (increments at each checkpoint)
+v_special_act:				equ __rs-1 ; $FFFFFE17 ; read if we only want the act number
+
+v_continues:				rs.b 1 ; $FFFFFE18 ; number of continues
+f_super_flag:				rs.b 1 ; $FFFFFE19 ; flag set when Sonic is super
+f_time_over:				rs.b 1 ; $FFFFFE1A ; time over flag
+v_ring_reward:				rs.b 1 ; $FFFFFE1B ; tracks which rewards have been given for rings 
+
+; HUD update flags
+f_hud_lives_update:			rs.b 1 ; $FFFFFE1C ; lives counter update flag
+v_hud_rings_update:			rs.b 1 ; $FFFFFE1D ; ring counter update flag - 1 = general update; $80 = reset to 0
+f_hud_time_update:			rs.b 1 ; $FFFFFE1E ; time counter update flag
+f_hud_score_update:			rs.b 1 ; $FFFFFE1F ; score counter update flag
+
+v_rings:					rs.w 1 ; $FFFFFE20 ; rings
+
+v_time:						rs.l 1 ; $FFFFFE22 ; time
+v_time_min:					equ __rs-3 ; $FFFFFE23 ; time - minutes
+v_time_sec:					equ __rs-2 ; $FFFFFE24 ; time - seconds
+v_time_frames:				equ __rs-1 ; $FFFFFE25 ; time - frames
+
+v_score:					rs.l 1 ; $FFFFFE26 ; score
+unused_FE2A:				rs.b 6 ; $FFFFFE2A-$FFFFFE2F ; unused
+
+v_last_lamppost:			rs.b 1 ; $FFFFFE30 ; max activated starpole ID in this act
 
 
+v_last_lamppost_lampcopy:		rs.b 1 ; $FFFFFE31 ; lamppost copy of v_last_lamppost
+v_x_pos_lampcopy:				rs.w 1 ; $FFFFFE32 ; lamppost copy of Sonic's x position
+v_y_pos_lampcopy:				rs.w 1 ; $FFFFFE34 ; lamppost copy of Sonic's y position
+v_rings_lampcopy:				rs.w 1 ; $FFFFFE36 ; lamppost copy of v_rings
+v_time_lampcopy:				rs.l 1 ; $FFFFFE38 ; lamppost copy of v_time
+v_ost_tile_lampcopy:			rs.w 1 ; $FFFFFE3C ; lamppost copy of main character's ost_tile RAM
+v_ost_top_solid_bit_lampcopy:	rs.w 1 ; $FFFFFE3E ; lamppost copy of main character's top_solid_bit
+v_camera_x_pos_lampcopy:		rs.w 1 ; $FFFFFE40 ; lamppost copy of v_camera_x_pos
+v_camera_y_pos_lampcopy:		rs.w 1 ; $FFFFFE42 ; lamppost copy of v_camera_y_pos
+v_bg1_x_pos_lampcopy:			rs.w 1 ; $FFFFFE44 ; lamppost copy of v_bg1_x_pos
+v_bg1_y_pos_lampcopy:			rs.w 1 ; $FFFFFE46 ; lamppost copy of v_bg1_y_pos
+v_bg2_x_pos_lampcopy:			rs.w 1 ; $FFFFFE48 ; lamppost copy of v_bg2_x_pos
+v_bg2_y_pos_lampcopy:			rs.w 1 ; $FFFFFE4A ; lamppost copy of v_bg2_y_pos
+v_bg3_x_pos_lampcopy:			rs.w 1 ; $FFFFFE4C ; lamppost copy of v_bg3_x_pos
+v_bg3_y_pos_lampcopy:			rs.w 1 ; $FFFFFE4E ; lamppost copy of v_bg3_y_pos
+v_water_height_normal_lampcopy:	rs.w 1 ; $FFFFFE50 ; lamppost copy of v_water_height_normal
+v_water_routine_lampcopy:		rs.b 1 ; $FFFFFE52 ; lamppost copy of v_water_routine
+f_water_pal_full_lampcopy:		rs.b 1 ; $FFFFFE53 ; lamppost copy of f_water_pal_full
+v_ring_reward_lampcopy:			rs.b 1 ; $FFFFFE54 ; lamppost copy of v_ring_reward
+v_ring_reward_lampcopy_2p:		rs.b 1 ; $FFFFFE55 ; stored, but never restored
+v_boundary_bottom_next_lampcopy:	rs.w 1 ; $FFFFFE56 ; lampppost copy of v_boundary_bottom_next
+v_dle_routine_lampcopy:			rs.b 1 ;  $FFFFFE58  ; lamppost copy of v_dle_routine
 
+unused_FE59:					rs.b 5 ; $FFFFFE59-$FFFFFE5D ; unused
+
+
+v_oscillating_direction:		rs.w 1 ; $FFFFFE5E ; bitfield for the direction values in the table below are moving - 0 = up; 1 = down
+				rsblock	oscillating_variables
+v_oscillating_table:			rs.l $10 ; $FFFFFE60 ; table of 16 oscillating values, for platform movement - 1 word for current value, 1 word for rate
+;v_oscillating_0_to_20:		equ v_oscillating_table
+;v_oscillating_0_to_30:		equ v_oscillating_table+4
+;v_oscillating_0_to_40:		equ v_oscillating_table+8
+;v_oscillating_0_to_60:		equ v_oscillating_table+$C
+;v_oscillating_0_to_40_fast:	equ v_oscillating_table+$10
+;v_oscillating_0_to_10:		equ v_oscillating_table+$14
+;v_oscillating_0_to_80_fast:	equ v_oscillating_table+$18
+;v_oscillating_0_to_80:		equ v_oscillating_table+$1C
+;v_oscillating_0_to_A0:		equ v_oscillating_table+$20
+;v_oscillating_0_to_A0_alt:	equ v_oscillating_table+$24
+;v_oscillating_0_to_40_alt:	equ v_oscillating_table+$28
+;v_oscillating_0_to_60_alt:	equ v_oscillating_table+$2C
+;v_oscillating_0_to_A0_fast:	equ v_oscillating_table+$30
+;v_oscillating_0_to_E0:		equ v_oscillating_table+$34
+
+; Synchronized animation counters: 0 = logspikes (unused), 1 = rings, 2 = unused, 3 = scattered rings
+
+v_syncani_0_time:		rs.b 1 ; $FFFFFEA0 ; synchronised sprite animation 0 - time until next frame
+v_syncani_0_frame:		rs.b 1 ; $FFFFFEA1 ; synchronised sprite animation 0 - current frame
+v_syncani_1_time:		rs.b 1 ; $FFFFFEA2 ; synchronised sprite animation 1 - time until next frame
+v_syncani_1_frame:		rs.b 1 ; $FFFFFEA3 ; synchronised sprite animation 1 - current frame
+v_syncani_2_time:		rs.b 1 ; $FFFFFEA4 ; synchronised sprite animation 2 - time until next frame
+v_syncani_2_frame:		rs.b 1 ; $FFFFFEA5 ; synchronised sprite animation 2 - current frame
+v_syncani_3_time:		rs.b 1 ; $FFFFFEA6 ; synchronised sprite animation 3 - time until next frame
+v_syncani_3_frame:		rs.b 1 ; $FFFFFEA7 ; synchronised sprite animation 3 - current frame
+v_syncani_3_accumulator:	rs.w 1 ; $FFFFFEA8 ; synchronised sprite animation 3 - v_syncani_3_time added to this value every frame
+
+				rs.b 6	; $FFFFFEA9-$FFFFFEAF ; seems unused, but cleared once
+				rsblockend	oscillating_variables
+				
+				rs.b $10	; $FFFFFEB0-$FFFFFEBF ; seems unused				
+
+				rsblock tails_speeds ; required for teleport swap table
+v_tails_max_speed:			rs.w 1 ; $FFFFFEC0 ; Tails' maximum speed
+v_tails_acceleration:		rs.w 1 ; $FFFFFEC2 ; Tails' acceleration
+v_tails_deceleration:		rs.w 1 ; $FFFFFEC4 ; Tails' deceleration		
+				rsblockend tails_speeds
+
+; Variables for the second player in 2P mode				
+v_lives_p2:					rs.b 1 ; $FFFFFEC6
+v_ring_reward_p2:			rs.b 1 ; $FFFFFEC7
+f_hud_lives_update_p2:		rs.b 1 ; $FFFFFEC8
+v_hud_rings_update_p2:		rs.b 1 ; $FFFFFEC9
+f_hud_time_update_p2:		rs.b 1 ; $FFFFFECA
+f_hud_score_update_p2:		rs.b 1 ; $FFFFFECB ; written to once, but never read
+f_time_over_p2:				rs.b 1 ; $FFFFFECC
+
+unused_FECD:				rs.b	3 ; $FFFFFECD-$FFFFFECF ; seems unused
+
+v_rings_p2:					rs.w	1 ; $FFFFFED0
+
+v_time_p2:					rs.l 1 ; $FFFFFED2 ; time
+v_time_min_p2:				equ __rs-3 ; $FFFFFED3 ; time - minutes
+v_time_sec_p2:				equ __rs-2 ; $FFFFFED4 ; time - seconds
+v_time_frames_p2:			equ __rs-1 ; $FFFFFED5 ; time - frames
+
+v_score_p2:					rs.l 1 ; $FFFFFED6		
+
+unused_FEDA:				rs.b 6	; $FFFFFEDA-$FFFFFEDF ; seems unused	
+
+; Lamppost store for player 2 in 2P mode				
+v_last_lamppost_p2:				rs.b 1 ; $FFFFFEE0
+v_last_lamppost_hit_p2_lampcopy:	rs.b 1 ; $FFFFFEE1
+v_x_pos_p2_lampcopy:			rs.w 1 ; $FFFFFEE2
+v_y_pos_p2_lampcopy:			rs.w 1 ; $FFFFFEE4
+v_rings_p2_lampcopy:			rs.w 1 ; $FFFFFEE6
+v_time_p2_lampcopy:				rs.l 1 ; $FFFFFEE8
+v_ost_tile_p2_lampcopy:			rs.w 1 ; $FFFFFEEC
+v_ost_top_solid_bit_p2_lampcopy:	rs.w 1 ; $FFFFFEEE
+
+; Additional 2P mode variables
+v_rings_collected_p1:		rs.w 1 ; $FFFFFEF0 ; number of rings collected by player 1 during an act in two player mode
+v_rings_collected_p2:		rs.w 1 ; $FFFFFEF2 ; same as above, but for player 2
+v_monitors_broken_p1:		rs.w 1 ; $FFFFFEF4 ; number of monitors broken by player 1 during an act in two player mode
+v_monitors_broken_p2:		rs.w 1 ; $FFFFFEF6 ; same as above, but for player 2
+v_loser_time_left:			rs.w 1 ; $FFFFFEF8 ; high byte is seconds, low byte is frames						
+	
+unused_FEFA:				rs.b $16	; $FFFFFEFA-$FFFFFF0F ; seems unused
+
+v_results_screen_2p:		rs.w 1 ; $FFFFFF10 ; 0 = act, 1 = zone, 2 = game, 3 = SS, 4 = SS all
+				
+unused_FF12:				rs.b $E	; $FFFFFF12-$FFFFFF1F ; seems unused	
+
+				rsblock 2p_results_data	; $18 (24) bytes, 6 bytes for each zone
+v_ehz_results_2p:			rs.b 6 ; $FFFFFF20
+v_mcz_results_2p:			rs.b 6 ; $FFFFFF26
+v_cnz_results_2p:			rs.b 6 ; $FFFFFF2C
+v_ss_results_2p:			rs.b 6 ; $FFFFFF32
+				rsblockend 2p_results_data	
+				
+v_ss_total_won:				rs.w 1 ; $FFFFFF38 ; high byte is player 1, low byte is player 2					
+
+unused_FF3A:				rs.b 6 ; $FFFFFF3A-$FFFFFF3F ; seems unused
+
+v_perfect_rings_left:		rs.w 1 ; $FFFFFF40 ; remaining number of rings in a level
+f_ss_perfect_rings_flag:	rs.w 1 ; $FFFFFF42 ; flag set if all rings in a special stage are collected
+
+unused_FF44:				rs.b 8	; $FFFFFF44-$FFFFFF4B ; seems unused
+
+v_credits_index:			rs.w 1 ; $FFFFFF4C ; current frame of the credits sequence
+f_slot_machine_flag:		equ v_credits_index ; flag indicating a CNZ slot machine is in use
+
+; CNZ slot machine variables; $12 values
+; v_slot_machine_routine and v_slot_machine_reward are accessed via absolute addressing, 
+; as they are used by the CNZ cage object to trigger the machines and retrieve the reward.
+; The others are only accessed via indirect displacement relative to v_slot_machine_variables.
+; I've only declared the former here, but the latter are nevertheless listed below in comments
+; for the sake of documentation and compatibility.
+
+
+v_slot_machine_variables:		rs.b $12 ; $FFFFFF4E
+v_slot_machine_routine:			equ v_slot_machine_variables ; $FFFFFF4E ; current routine of the slot machine psuedoobject
+;v_slot_machine_timer:			equ __rs-$10 ; v_slot_machine_variables+1 ; $FFFFFF4F 
+
+;unused_FF50:					equ __rs-$F ; v_slot_machine_variables+2	1 ; $FFFFFF50 ; seems unused except for 1 write
+
+;v_slot_machine_index:			equ __rs-$E ; v_slot_machine_variables+3 ; $FFFFFF51
+v_slot_machine_reward:			equ __rs-$D ; v_slot_machine_variables+4 ; $FFFFFF52
+
+;v_slot_machine_slot1_pos:		equ __rs-$C ; v_slot_machine_variables+5 ; $FFFFFF54
+;v_slot_machine_slot1_speed:	equ __rs-$A ; v_slot_machine_variables+7 ; $FFFFFF56
+;v_slot_machine_slot1_rout:		equ __rs-9 ; v_slot_machine_variables+8  ; $FFFFFF57
+;v_slot_machine_slot2_pos:		equ __rs-8 ; v_slot_machine_variables+9  ; $FFFFFF58
+;v_slot_machine_slot2_speed:	equ __rs-6 ; v_slot_machine_variables+$B ; $FFFFFF5A
+;v_slot_machine_slot2_rout:		equ __rs-5 ; v_slot_machine_variables+$C ; $FFFFFF5B
+;v_slot_machine_slot3_pos:		equ __rs-4 ; v_slot_machine_variables+$D ; $FFFFFF5C
+;v_slot_machine_slot3_speed:	equ __rs-2 ; v_slot_machine_variables+$F ; $FFFFFF5E
+;v_slot_machine_slot3_rout:		equ __rs-1 ; v_slot_machine_variables+$10 ; $FFFFFF5F
+
+unused_FF6F:					rs.b $10	; $FFFFFF60-$FFFFFF6F ; seems unused
+				
+v_player_mode:					rs.w 1 ; $FFFFFF70 ; current player mode; 0 = Sonic and Tails, 1 = Sonic, 2 = Tails
+v_player_option:				rs.w 1 ; $FFFFFF72 ; player mode option selected in options menu;  0 = Sonic and Tails, 1 = Sonic, 2 = Tails
+f_two_player_monitors:			rs.w 1 ; $FFFFFF74 ; flag for monitor contents in 2P mode; 0 = all items, 1 = teleport only 
+
+unused_FF76:					rs.b $A	; $FFFFFF76-$FFFFFF7F ; seems unused	
+
+v_levelselect_hold_delay:		rs.w 1 ; $FFFFFF80
+v_level_select_zone:			rs.w 1 ; $FFFFFF82 ;  currently selected zone in level select
+v_sound_test_sound:				rs.w 1 ; $FFFFFF84 ; current sound test selection
+v_title_screen_option:			rs.b 1 ; $FFFFFF86 ; selected option on the title screen
+	
+unused_FF87:					rs.b 1 ; $FFFFFF87 ; unused
+v_zone_2p:						rs.w 1 ; $FFFFFF88; high byte is current zone number, low byte is current act number ; read as a byte if we only want the zone
+v_act_2p:						equ __rs-1 ; $FFFFFF89 ; read if we only want the act number
+f_two_player_mode_copy:			rs.w 1 ; $FFFFFF8A ; second flag set to indicate two player mode; read twice in Special Stage
+v_options_menu_box:				rs.b 1 ; $FFFFFF8C ; currently selected box in options menu
+
+unused_FF8D:					rs.b 1 ; $FFFFFF8D ; unused
+
+v_total_bonus_countdown:		rs.w 1 ; $FFFFFF8E
+		
 ;v_console_region:           rs.b    1   ; $FFFFFFF8 ; Mega Drive console type - 0 = JP; $80 = US/EU; +0 = NTSC; +$40 = PAL
-
 ;v_checksum_pass:            rs.l    1   ; $FFFFFFFC ; set to the text string "init" when checksum is passed
 
+
+; Show RAM usage and stop compilation if it overflows.
+
+ram_used:			equ __rs
+ram_final:			equ (ram_used-1)&$FFFF
+		if ram_used > 0
+		inform	3,"RAM usage exceeds maximum by $%h bytes.",ram_used
+		else
+		inform	0,"0-$%h bytes of RAM used with $%h bytes to spare.",ram_final,$FFFF-ram_final
+		endc
 
 v_ss_shared_ram:           equ v_ost_level_only ; $FFFFD000	
 ;	v_ss_shared_ram_end:   equ v_vdp_dma_buffer
